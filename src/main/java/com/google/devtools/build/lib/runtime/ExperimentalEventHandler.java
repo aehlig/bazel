4e842034f1a2a774a3e64ddf8b6d1db018dfe0d8
Klaus Aehlig <aehlig@google.com>
2016-02-22 16:37:34 +0100

    Experimental UI: report ERROR/WARNING/INFO as usual
    While the eventual goal is to improve the reporting of
    those events, for the time being, report them as usual,
    so that the experimental UI is minimally usable for building.

--- src/main/java/com/google/devtools/build/lib/runtime/ExperimentalEventHandler.java
+++ src/main/java/com/google/devtools/build/lib/runtime/ExperimentalEventHandler.java
@@ -72,12 +72,41 @@ public class ExperimentalEventHandler extends BlazeCommandEventHandler {
             stream.write(new byte[] { 10, 13 });
             stream.flush();
             break;
+          case ERROR:
+          case WARNING:
+          case INFO:
+            setEventKindColor(event.getKind());
+            terminal.writeString(event.getKind() + ": ");
+            terminal.resetTerminal();
+            if (event.getLocation() != null) {
+              terminal.writeString(event.getLocation() + ": ");
+            }
+            if (event.getMessage() != null) {
+              terminal.writeString(event.getMessage());
+            }
+            crlf();
+            break;
         }
       }
       addProgressBar();
       terminal.flush();
     } catch (IOException e) {
       LOG.warning("IO Error writing to output stream: " + e);
+    }
+  }
+
+  private void setEventKindColor(EventKind kind) throws IOException {
+    switch(kind) {
+      case ERROR:
+        terminal.textRed();
+        terminal.textBold();
+        break;
+      case WARNING:
+        terminal.textMagenta();
+        break;
+      case INFO:
+        terminal.textGreen();
+        break;
     }
   }
 
@@ -90,16 +119,7 @@ public class ExperimentalEventHandler extends BlazeCommandEventHandler {
   @Subscribe
   public void loadingComplete(LoadingPhaseCompleteEvent event) {
     stateTracker.loadingComplete(event);
-    try {
-      clearProgressBar();
-      terminal.textGreen();
-      terminal.writeString("INFO:");
-      terminal.resetTerminal();
-      terminal.writeString(" " + event.getTargets().size() + " Target(s)\n");
-      addProgressBar();
-    } catch (IOException e) {
-      LOG.warning("IO Error writing to output stream: " + e);
-    }
+    refresh();
   }
 
   @Subscribe
