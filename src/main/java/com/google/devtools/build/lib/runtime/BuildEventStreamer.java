731da6ae4d6c2a8e8fe7b4e25d9f49fb04aaddd6
Klaus Aehlig <aehlig@google.com>
2016-04-21 16:49:23 +0200

    Add a build-event streamer class
    The BuildEventStreamer will listen for BuildEvents and stream them
    to the provided transports. It also ensures events are properly chained:
    for unsolicited events, it will add progress events to chain them and
    at the end of a build it closes all announced but not produced events
    as aborted.
    
    Change-Id: I623b582657573fe1288821c96f084e0ab0bca4d4

---
+++ src/main/java/com/google/devtools/build/lib/runtime/BuildEventStreamer.java
@@ -0,0 +1,138 @@
+// Copyright 2016 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.runtime;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.eventbus.Subscribe;
+import com.google.devtools.build.lib.analysis.NoBuildEvent;
+import com.google.devtools.build.lib.buildeventstream.BuildEvent;
+import com.google.devtools.build.lib.buildeventstream.BuildEventId;
+import com.google.devtools.build.lib.buildeventstream.BuildEventTransport;
+import com.google.devtools.build.lib.buildeventstream.GenericBuildEvent;
+import com.google.devtools.build.lib.buildeventstream.ProgressEvent;
+import com.google.devtools.build.lib.buildtool.buildevent.BuildCompleteEvent;
+import com.google.devtools.build.lib.events.Event;
+import com.google.devtools.build.lib.events.EventHandler;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Listen for {@link BuildEvent} and stream them to the provided {@link BuildEventTransport}.
+ */
+public class BuildEventStreamer implements EventHandler {
+  private final Collection<BuildEventTransport> transports;
+  private Set<BuildEventId> announcedEvents;
+  private int progressCount;
+
+  public BuildEventStreamer(Collection<BuildEventTransport> transports) {
+    this.transports = transports;
+    this.announcedEvents = null;
+    this.progressCount = 0;
+  }
+
+  /**
+   * Post a new event to all transports; simultaneously keep track of the events we announce to
+   * still come.
+   *
+   * Moreover, link unannounced events to the progress stream; we only expect failure events to
+   * come before their parents.
+   */
+  private void post(BuildEvent event) {
+    BuildEvent linkEvent = null;
+    BuildEventId id = event.getEventId();
+
+    synchronized(this) {
+      if (announcedEvents == null) {
+        announcedEvents = new HashSet<>();
+        if (!event.getChildrenEvents().contains(ProgressEvent.INITIAL_PROGRESS_UPDATE)) {
+          linkEvent = ProgressEvent.progressChainIn(progressCount, event.getEventId());
+          progressCount++;
+          for (BuildEventId childId : linkEvent.getChildrenEvents()) {
+            announcedEvents.add(childId);
+          }
+          announcedEvents.remove(linkEvent.getEventId());
+        }
+      } else {
+        if (!announcedEvents.contains(id)) {
+          linkEvent = ProgressEvent.progressChainIn(progressCount, id);
+          progressCount++;
+          for (BuildEventId childId : linkEvent.getChildrenEvents()) {
+            announcedEvents.add(childId);
+          }
+          announcedEvents.remove(linkEvent.getEventId());
+        }
+        announcedEvents.remove(id);
+      }
+      for (BuildEventId childId : event.getChildrenEvents()) {
+        announcedEvents.add(childId);
+      }
+    }
+
+    for (BuildEventTransport transport : transports) {
+      if (linkEvent != null) {
+        transport.sendBuildEvent(linkEvent);
+      }
+      transport.sendBuildEvent(event);
+    }
+  }
+
+  /**
+   * Clear all events that are still pending; events not naturally
+   * closed by the expected event normally only occur if the build
+   * is aborted.
+   */
+  private void clearPendingEvents() {
+    if (announcedEvents != null) {
+      // create a copy of the identifiers to clear, as the post method
+      // will change the set of already announced events.
+      ImmutableSet<BuildEventId> ids;
+      synchronized(this) {
+        ids = ImmutableSet.copyOf(announcedEvents);
+      }
+      for (BuildEventId id : ids) {
+        post(GenericBuildEvent.buildAborted(id));
+      }
+    }
+  }
+
+  private void close() {
+    for (BuildEventTransport transport : transports) {
+      transport.close();
+    }
+  }
+
+  @Override
+  public void handle(Event event) {}
+
+  @Subscribe
+  public void noBuild(NoBuildEvent event) {
+    close();
+  }
+
+  @Subscribe
+  public void buildComplete(BuildCompleteEvent event) {
+    post(ProgressEvent.finalProgressUpdate(progressCount));
+    clearPendingEvents();
+    close();
+  }
+
+  @Subscribe
+  public void buildEvent(BuildEvent event) {
+    post(event);
+  }
+}
+
