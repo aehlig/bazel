8142d5496d70f1d363335b55ee74cefda254c6ef
Klaus Aehlig <aehlig@google.com>
2016-04-28 15:24:39 +0200

    Show the starting of the build in the build event stream
    Make the BuildStartingEvent visible in the event stream by making it
    an instance of the BuildEvent interface. It is the initial event of the
    build event stream hence it also announces that progress updates will
    follow. Its regular decedents are the expansion of the target patterns
    provided at the request.
    
    Change-Id: I237d8559b71ac82b10fdc492492b8435d6d1483f

--- src/main/java/com/google/devtools/build/lib/buildtool/buildevent/BuildStartingEvent.java
+++ src/main/java/com/google/devtools/build/lib/buildtool/buildevent/BuildStartingEvent.java
@@ -14,16 +14,28 @@
 
 package com.google.devtools.build.lib.buildtool.buildevent;
 
+import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.analysis.BlazeVersionInfo;
 import com.google.devtools.build.lib.buildtool.BuildRequest;
+import com.google.devtools.build.lib.buildeventstream.BuildEvent;
+import com.google.devtools.build.lib.buildeventstream.BuildEventId;
+import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos;
+import com.google.devtools.build.lib.buildeventstream.GenericBuildEvent;
+import com.google.devtools.build.lib.buildeventstream.ProgressEvent;
+import com.google.devtools.build.lib.runtime.CommandEnvironment;
+
+import java.util.Collection;
 
 /**
  * This event is fired from BuildTool#startRequest().
  * At this point, the set of target patters are known, but have
  * yet to be parsed.
  */
-public class BuildStartingEvent {
+public class BuildStartingEvent implements BuildEvent {
   private final String outputFileSystem;
   private final BuildRequest request;
+  private final String workspace;
+  private final String pwd;
 
   /**
    * Construct the BuildStartingEvent.
@@ -32,7 +44,26 @@ public class BuildStartingEvent {
   public BuildStartingEvent(String outputFileSystem, BuildRequest request) {
     this.outputFileSystem = outputFileSystem;
     this.request = request;
+    this.workspace = null;
+    this.pwd = null;
   }
+
+  /**
+   * Construct the BuildStartingEvent
+   * @param request the build request.
+   * @param env the environment of the request invocation.
+   */
+  public BuildStartingEvent(CommandEnvironment env, BuildRequest request) {
+    this.outputFileSystem = env.determineOutputFileSystem();
+    this.request = request;
+    if (env.getDirectories().getWorkspace() != null) {
+      this.workspace = env.getDirectories().getWorkspace().toString();
+    } else {
+      this.workspace = null;
+    }
+    this.pwd = env.getWorkingDirectory().toString();
+  }
+
 
   /**
    * @return the output file system.
@@ -46,5 +77,35 @@ public class BuildStartingEvent {
    */
   public BuildRequest getRequest() {
     return request;
+  }
+
+  @Override
+  public BuildEventId getEventId() {
+    return new BuildEventId(BuildEventStreamProtos.BuildEventType.BUILD_STARTED, "", 0);
+  }
+
+  @Override
+  public Collection<BuildEventId> getChildrenEvents() {
+    return ImmutableList.of(
+        ProgressEvent.INITIAL_PROGRESS_UPDATE,
+        BuildEventId.targetPatternExpanded(request.getTargets()));
+  }
+
+  @Override
+  public BuildEventStreamProtos.BuildEvent asStreamProto() {
+    BuildEventStreamProtos.BuildStarted.Builder started =
+        BuildEventStreamProtos.BuildStarted.newBuilder()
+        .setUuid(request.getId().toString())
+        .setStartTimeMilis(request.getStartTime())
+        .setBuildToolVersion(BlazeVersionInfo.instance().getVersion())
+        .setOptionsDescription(request.getOptionsDescription())
+        .setCommand(request.getCommandName());
+    if (pwd != null) {
+      started.setWorkingDirectory(pwd);
+    }
+    if (workspace != null) {
+      started.setWorkspaceDirectory(workspace);
+    }
+    return GenericBuildEvent.protoChaining(this).setStarted(started.build()).build();
   }
 }
